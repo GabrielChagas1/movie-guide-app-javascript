120096fcfcead52e8f474d3e85ab6d8bb885bc28
Gabriel Chagas <gabrielserq@outlook.com>
2022-07-02 16:27:30 -0300

    chore: creating initial references js.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Initial References
+let movieNameRef = document.getElementById('movie-name');
+let searchBtn = document.getElementById('search-btn');
+let result = document.getElementById('result');
+
